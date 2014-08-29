0a79653864ada8a45a6389b5d478815a76b2e477
p1ch_jp <p1ch.jp@gmail.com>
2014-08-29 13:14:01 +0900

    comit

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 var http = require('http');
 var fs = require('fs');
-var index = require('./core/index.js');
-var post = require('./core/post.js');
+var router = require('./core/router.js');
 var jade = require('jade');
 
 var server = http.createServer();
-server.on('request', doRequest);
+server.on('request', router.func);
 server.listen(5000, function () {
   console.log('Server running! on localhost:5000...');
 });
-
-function doRequest (req, res) {
-  switch (req.url) {
-    case '/':
-      index.func(req, res);
-      break;
-    case '/inari/':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("Welcome to inari");
-      res.end();
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write("404 Not Found!!");
-      res.end();
-      break;
-  }
-}
--- core/router.js
+++ core/router.js
@@ -3,3 +3,24 @@ var fs = require('fs');
 var jade = require('jade');
 var sqlite3 = require('sqlite3').verbose();
 var async = require('async');
+
+var index = require('core/index.js');
+var post = require('core/post.js');
+
+exports.func = function doReq (req, res) {
+  switch (req.url) {
+    case '/':
+      index.func(req, res);
+      break;
+    case '/inari/':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write("Welcome to inari");
+      res.end();
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write("404 Not Found!!");
+      res.end();
+      break;
+  }
+}
